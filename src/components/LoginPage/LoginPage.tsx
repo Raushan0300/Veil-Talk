55c3dd0f6c344f167eaf00086adbadc9ec0a0a19
Raushan Kumar <raushankumar2909@gmail.com>
2024-02-18 20:23:58 +0530

    login page ui completed

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -2,8 +2,12 @@ import './LoginPage.css';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='login-main-div'>
+        <div className='login-text'>Welcome to Veil Talk</div>
+        <input type="email" className='email-input' placeholder='Email' />
+        <button className='login-btn'>Log In</button>
+    </div>
   )
-}
+};
 
-export default LoginPage
+export default LoginPage;
