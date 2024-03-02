52b5bbdc0b88110f9198bb1b250de04c2d8182b4
Raushan Kumar <raushankumar2909@gmail.com>
2024-03-02 13:56:29 +0530

    api changed

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,13 +1,35 @@
-import './LoginPage.css';
+import axios from "axios";
+import "./LoginPage.css";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleLogin = async () => {
+    const response = await axios.post("https://veil-talk-backend.vercel.app/api/register", { email, password });
+    const data=response.data;
+    console.log(data);
+  };
   return (
-    <div className='login-main-div'>
-        <div className='login-text'>Welcome to Veil Talk</div>
-        <input type="email" className='email-input' placeholder='Email' />
-        <button className='login-btn'>Log In</button>
+    <div className="login-main-div">
+      <div className="login-text">Welcome to Veil Talk</div>
+      <input
+        type="email"
+        className="email-input"
+        placeholder="Email"
+        value={email}
+        onChange={(e)=>{setEmail(e.target.value)}}
+      />
+      <input
+        type="password"
+        className="email-input"
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>{setPassword(e.target.value)}}
+      />
+      <button className="login-btn" onClick={()=>{handleLogin()}}>Log In</button>
     </div>
-  )
+  );
 };
 
 export default LoginPage;
